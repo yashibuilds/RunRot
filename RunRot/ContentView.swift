4c9a51e76c7047cfd8f115a7113298c9becdc4a3
yashibuilds <yashibuilds@gmail.com>
2025-12-27 12:58:39 -0500

    Initial Commit

---
+++ RunRot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunRot
+//
+//  Created by Yashi Surapaneni on 12/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunRot/RunRotApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunRotApp.swift
+//  RunRot
+//
+//  Created by Yashi Surapaneni on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct RunRotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
